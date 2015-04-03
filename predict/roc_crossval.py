cb61f435caa2af58935361b59699d61d53dd94b6
sean <sean.johnst@gmail.com>
2015-04-02 22:41:06 -0500

    mean ROC for one class split

--- predict/roc_crossval.py
+++ predict/roc_crossval.py
@@ -4,23 +4,16 @@ import numpy as np
 from scipy import interp
 import matplotlib.pyplot as plt
 
-from sklearn import svm, datasets
+from sklearn import svm
 from sklearn.metrics import roc_curve, auc
 from sklearn.cross_validation import StratifiedKFold
 from mut_group_pred_pack import *
+from cross_val import get_x_y_scorer_folds
 
 mut_list = get_full_mut_list()
 pten_mutations = MutationGroup(mut_list)
-fliplist = []
-for i in pten_mutations.category_2_list:
-    if i == 0:
-        fliplist.append(1)
-    else:
-        fliplist.append(0)
 
-fliplist = np.array(fliplist)
-
-X, y = pten_mutations.scaled_vector_array, fliplist
+X, y, scorer, folds = get_x_y_scorer_folds(pten_mutations, 2)
 
 n_samples, n_features = X.shape
 
@@ -28,9 +21,16 @@ n_samples, n_features = X.shape
 
 # run classifier with cross-validation and plot ROC curves
 
-cv = StratifiedKFold(y, n_folds=6, shuffle=True)
-classifier = svm.SVC(kernel='rbf', C=600, gamma=0.001, probability=True, \
-                                                            random_state=0)
+param_grid = [
+    {'C': [.1, 1, 10, 100, 1000 ],
+     'gamma': [0.1, 0.01, 0.001, 0.0001]},
+]
+
+cv = StratifiedKFold(y, n_folds=6, shuffle=True, random_state=0)
+svc = svm.SVC(kernel='rbf', cache_size=2000, class_weight='auto',
+              probability=True)
+classifier = GridSearchCV(svc, param_grid=param_grid, scoring=scorer,
+                          cv=folds, n_jobs=-1)
 
 mean_tpr = 0.0
 mean_fpr = np.linspace(0,1,100)
@@ -43,7 +43,7 @@ for i, (train, test) in enumerate(cv):
     mean_tpr += interp(mean_fpr, fpr, tpr)
     mean_tpr[0] = 0.0
     roc_auc = auc(fpr, tpr)
-    plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+    #plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
 
 plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
 
